2e0e3e49b77f1aaa4d1a2c423e3a36918944c9be
Devin Simpson <devin2806@gmail.com>
2021-07-10 17:44:14 -0400

    changes made to display times and input boxes, changes color with past,present, future times

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //formatting the displayed current day
 var now = dayjs().format('MMM-DD-YYYY ddd');
-
+var currentTime = moment().hour();
+console.log(currentTime);
 $('#currentDay').text(now);
 
 var workHours = [
@@ -16,7 +17,48 @@ var workHours = [
         time: 11,
         timeDisplay: '11:00 am',
     },
+    {
+        time: 12,
+        timeDisplay: '12:00 pm',
+    },
+    {
+        time: 10,
+        timeDisplay: '10:00 pm',
+    },
+    {
+        time: 11,
+        timeDisplay: '11:00 am',
+    },
+    {
+        time: 9,
+        timeDisplay: '9:00 am',
+    },
+    {
+        time: 10,
+        timeDisplay: '10:00 am',
+    },
+    {
+        time: 11,
+        timeDisplay: '11:00 am',
+    }
 ];
+
+var textAreaBlock = $(".description");
+console.log(textAreaBlock);
+
+textAreaBlock.each(function(){
+    console.log($(this).attr("id"));
+    var timeBlockHour = parseInt($(this).attr("id"));
+    if(timeBlockHour === currentTime) {
+        console.log("is equal")
+        $(this).addClass("present");
+    } else if (timeBlockHour < currentTime) {
+        $(this).addClass("past");
+    } else {
+        $(this).addClass("future");
+    }
+
+})
 
 
 
